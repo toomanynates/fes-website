c934e76650f217b0a55f96df8c7c6545bde34368
Nate Leavitt <toomanynates@yahoo.com>
2026-08-05 17:05:40 -0700

    Making the page interactive so i can browse old projects

---
+++ index.js
@@ -0,0 +1,7 @@
+function openWindow(strUrl) {
+  window.open(strUrl, '_blank'); // Replace the URL with your desired page
+}
+
+console.log("it's working ");
+
+//document.getElementById('project1').addEventListener('click', openNewPage);
